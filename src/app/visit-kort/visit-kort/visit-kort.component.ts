468f59fa0e257b7110595d587d9216eaaaa1f030
Joachim <joachimaberg@gmail.com>
2017-09-19 22:36:43 +0200

    Bytt ut visitkort mot ett nytt hundlöst. ändrat från css till scss

--- src/app/visit-kort/visit-kort/visit-kort.component.ts
+++ src/app/visit-kort/visit-kort/visit-kort.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'vk-visit-kort',
   templateUrl: './visit-kort.component.html',
-  styleUrls: ['./visit-kort.component.css']
+  styleUrls: ['./visit-kort.component.scss']
 })
 export class VisitKortComponent implements OnInit {
 
